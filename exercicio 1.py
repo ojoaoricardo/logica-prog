dc02f743b733031e49fd50e809ab96429cf74cc2
ojoaoricardo <joaoricardogermano94@outlook.com>
2025-05-12 11:56:14 -0300

    Create exercicio 1.py
    Create File Exercicio 1

---
+++ exercicio 1.py
@@ -0,0 +1,10 @@
+nota1 = float ( input ( "Digite sua primeira nota: " ))
+nota2 = float ( input ( "Digite sua segunda nota: " ))
+nota3 = float ( input ( "Digite sua terceira nota: " ))
+nota4 = float ( input ( "Digite sua nota de quarta: " ))
+media = (nota1 + nota2 + nota3 + nota4) / 4 
+print ( "Sua média é: " ,media)
+
+if media >= 7 :
+     print ( "Aprovado!" )
+else :  print ( "Reprovado!" )
